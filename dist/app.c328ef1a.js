13ba15110180cccd7209b462e2092fbe7c2c7600
louannem <84766775+louannem@users.noreply.github.com>
2021-10-10 15:12:37 +0200

    Ajout du bundle

--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -2374,7 +2374,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55378" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51133" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
